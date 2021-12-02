8ec905f001878942e674128272383a5bfa10942e
192114 <shadowSun192114@gmail.com>
2021-12-02 17:30:54 +0800

    动态导入 路由懒加载

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
 import {createApp} from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
-import Login from './pages/Login.vue'
-import Register from './pages/Register.vue'
-import UserInfo from './pages/UserInfo.vue'
-import TodoList from './pages/TodoList.vue'
-import EditTodo from './pages/EditTodo.vue'
+
+// 动态导入
+// import Login from './pages/Login.vue'
+// import Register from './pages/Register.vue'
+// import UserInfo from './pages/UserInfo.vue'
+// import TodoList from './pages/TodoList.vue'
+// import EditTodo from './pages/EditTodo.vue'
+
+const Login = () => import('./pages/Login.vue')
+const Register = () => import('./pages/Register.vue')
+const UserInfo = () => import('./pages/UserInfo.vue')
+const TodoList = () => import('./pages/TodoList.vue')
+const EditTodo = () => import('./pages/EditTodo.vue')
 
 import './assets/css/root.css'
 import './assets/css/common.css'
